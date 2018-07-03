929ab41a04be9338507af7260a9afd8649a4f67d
BarbaraKosela <38699532+BarbaraKosela@users.noreply.github.com>
2018-07-03 14:33:22 -0300

    Inicialização de exemplo

---
+++ EXEMPLO JOIN.sql
@@ -0,0 +1,9 @@
+﻿	SELECT pokemons.name 'Pokemon', categories.name 'Categoria' 
+	FROM pokemons JOIN categories ON (categories.id = pokemons.id_categoryI) 
+	WHERE pokemons.name = 'Bulbasaur';
+ 
+ 
+ 
+	SELECT categories.name 'Categoria', pokemons.name 'Pokemon' 
+	FROM categories JOIN pokemons ON (categories.id = pokemons.id_categoryI) 
+	WHERE categories.name = 'Poison' ORDER BY categories.name, pokemons.name; 
